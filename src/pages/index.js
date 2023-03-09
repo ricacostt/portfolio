be67e03650dc2314c4f20a7dbeda3818d95d501d
ricacostt <ricacostt@gmail.com>
2023-03-09 12:53:05 +0000

    setting up the project

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Federica Costantino</title>
+        <meta name="description" content="portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/seasvg.svg" />
+      </Head>
+      <main className={styles.main}>
+        <h1>Federica</h1>
+      </main>
+    </>
+  )
+}
