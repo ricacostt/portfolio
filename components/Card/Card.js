0d96fb5deb3d7287e996097123364e98508902c5
ricacostt <ricacostt@gmail.com>
2023-03-11 11:18:51 +0000

    start on the card style

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 function Card() {
   return (
     <div className='flex flex-row p-6 xsm:p-10 max-w-screen-xl mr-auto ml-auto'>
    
-<div className=" max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+{/* <div className=" max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <svg className="w-10 h-10 mb-2 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z" clip-rule="evenodd"></path><path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path></svg>
     <a href="#">
         <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
@@ -15,15 +16,29 @@ function Card() {
         See our guideline
         <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
     </a>
-</div>
-<div className="card w-96 bg-base-100 shadow-xl image-full">
-  <figure><Image src="/stokka.png" alt="Shoes" width="400" height='20' /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+</div> */}
+<div className="card image-full max-w-sm border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  <figure><Image src="/stokka.png" alt="stokka landing page" width="400" height='20' /></figure>
+  <div className="card-body ">
+    <h2 className="text-2xl font-semibold tracking-tight  text-textdark">Stokka</h2>
+    <p className='text-textdark' >A mobile-first website where the user can filter board games based on their needs and preferences and can be part of an active community.</p>
+    <div className='card-actions justify-start'>
+    <a href="https://stokka.netlify.app/" target="_blank"
+              rel="noopener noreferrer" className="inline-flex items-center text-textdark hover:underline text-base">
+        Live Demo 
+        <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
+    </a>
     </div>
+    <div className='card-actions justify-start'>
+    <a href="https://stokka.netlify.app/" target="_blank"
+              rel="noopener noreferrer" className="inline-flex items-center text-textdark hover:underline text-base">
+        Code 
+    </a>
+     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+    </div>
+    {/* <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div> */}
   </div>
 </div>
 
